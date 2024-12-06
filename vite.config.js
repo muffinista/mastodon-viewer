1565a45036ebc2414809335ad19107ad50224915
Colin Mitchell <colin@muffinlabs.com>
2024-12-06 15:30:42 -0500

    Disable single file for now

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
-import { viteSingleFile } from 'vite-plugin-singlefile';
+// import { viteSingleFile } from 'vite-plugin-singlefile';
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [svelte(), viteSingleFile()],
+	plugins: [svelte()], // , viteSingleFile()
 	optimizeDeps: {
 		allowNodeBuiltins: ['pouchdb-browser', 'pouchdb-utils']
 	}
