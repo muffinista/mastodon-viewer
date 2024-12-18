b2e500736635e0987c64cb4cf3c2b6b31d744d53
Colin Mitchell <colin@muffinlabs.com>
2024-12-17 21:06:00 -0500

    Add favicon
    Peak modern web development

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,22 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+
 // import { viteSingleFile } from 'vite-plugin-singlefile';
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [svelte()], // , viteSingleFile()
+	plugins: [
+		svelte(),
+		viteStaticCopy({
+      targets: [
+        {
+          src: 'static/favicon.ico',
+          dest: 'favicon.ico'
+        }
+      ]
+    })
+	], // , viteSingleFile()
 	optimizeDeps: {
 		allowNodeBuiltins: ['pouchdb-browser', 'pouchdb-utils']
 	},
